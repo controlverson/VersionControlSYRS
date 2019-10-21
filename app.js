2258f03020f3052a080aa46408263988850879e7
shreya730 <shreyamishra839@gmail.com>
2019-10-21 14:28:31 +0530

    UI changes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+
 var express=require('express');
 const mysql= require('mysql');
+
 //create mysql connection
 const db= mysql.createConnection({
   host:'localhost',
@@ -8,6 +10,17 @@ const db= mysql.createConnection({
   database:'version_control',
   multipleStatements:'true'
 });
+
+var message;
+var fnumber;
+var femail;
+
+var uid;
+var uname;
+var uemail;
+var ucontact;
+var upass;
+
 //connect
 db.connect((err)=>{
   if(err){
@@ -16,13 +29,13 @@ db.connect((err)=>{
   console.log('MySql Connected...');
 });
 var app=express();
-/*app.get('/createdb',(req,res)=>{
-  let sql= 'SELECT * from user';
+app.get('/createdb',(req,res)=>{
+  let sql= 'insert into feedback values(29,"h04@gmail.com",'+ message +');';
   db.query(sql,(err,result)=>{
     if(err) throw err;
-    console.log(result);
+    res.send("inserted");
   });
-});*/
+});
 app.use(express.static(__dirname + '/public'));
 app.set('view engine','ejs');
 
@@ -35,17 +48,38 @@ app.get('/',function(req,res){
 });
 app.get('/views/feedback',function(req,res){
   res.render('feedback');
-  //let sql= 'INSERT into feedback(U_ID,email,feedback) values(?,?,?)';
-  //db.query(sql,(err,result)=>{
-    //if(err) throw err;
-    //console.log(result);
-    //res.send('result');
-  //});
-});
-app.post('/views/feedback', (req,res) => {
-  res.send(req.body);
 });
 
+app.get('/views/signup',function(req,res){
+  res.render('signup');
+});
+app.post('/views/feedback', (req,res) => {
+    message="'"+req.body.feedback+"'";
+    fnumber="'"+req.body.num+"'";
+    femail="'"+req.body.email+"'";
+    let sql= 'insert into feedback values('+ fnumber +',' + femail + ','+ message +');';
+  db.query(sql,(err,result)=>{
+    if(err) throw err;
+    console.log("inserted");
+  });
+});
+
+app.post('/views/signup', (req,res) => {
+  uid="'"+req.body.ID+"'";
+  uname="'"+req.body.name+"'";
+  uemail="'"+req.body.email+"'";
+  ucontact="'"+req.body.number+"'";
+  upass="'"+req.body.password+"'";
+  
+  let sql= 'insert into user values('+ uid +',' + uname + ','+ uemail +','+ ucontact +','+ upass +');';
+db.query(sql,(err,result)=>{
+  if(err){ 
+    res.send("INVALID DETAILS: Try Again!");
+    throw err;
+  }
+  console.log("inserted");
+});
+});
 
 app.use(function(req,res,next){
     console.log("looking for url"+ req.url);
